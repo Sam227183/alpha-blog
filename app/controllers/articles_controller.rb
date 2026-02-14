95a55960d47a21c75bfa138069083873c68e4a15
Hansaem Kim <dalbyul9062@gmail.com>
2026-02-13 17:14:21 -0700

    Forms created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,13 @@ class ArticlesController < ApplicationController
   end
 
   def index
-    @articles = Article.all  
+    @articles = Article.all
+  end
+
+  def new
+  end
+
+  def create
+    render plain: params[:article]
   end
 end
