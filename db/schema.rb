f809c8d37c20cffc4af537728b1e46b846eb5e23
Hansaem Kim <dalbyul9062@gmail.com>
2026-03-11 14:10:43 -0600

    Added tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_26_025810) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_10_212312) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at"
     t.text "description"
@@ -19,7 +19,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_26_025810) do
     t.integer "user_id"
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.string "email"
     t.string "password_digest"
